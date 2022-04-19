56c6930b3a60387ba4fb5b6c0aa8a7b2a4e9eb83
SydVic <fabrizio.bocca27@gmail.com>
2022-04-19 15:31:56 +0200

    aggiunto jsnack4

---
+++ JSnack4/js/script.js
@@ -0,0 +1,16 @@
+// chiedo un numero di 4 cifre all'utent
+const userNumber = prompt("Ciao!Dimmi un numero di 4 cifre");
+console.log(userNumber, typeof(userNumber));
+
+// lo scompongo in quattro elementi
+let sumModule = [];
+sumModule = userNumber.split("");
+console.log(sumModule, typeof(sumModule));
+
+// calcolo la somma dei singoli numeri
+summedModule = 0;
+for (let i = 0; i < sumModule.length; i++) {
+    summedModule = summedModule + parseInt(sumModule[i]);
+}
+
+console.log(summedModule);
