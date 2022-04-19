956e9d33c17620a09f2c938b125313d15f876959
SydVic <fabrizio.bocca27@gmail.com>
2022-04-19 14:34:57 +0200

    aggiunto jsnack3

---
+++ JSnack3/js/script.js
@@ -0,0 +1,14 @@
+let sum = 0;
+console.log(sum);
+
+let average = 0;
+
+for ( let i = 1; i <= 10; i++) {
+    console.log(i);
+    sum = sum + i;
+}
+
+console.log("Somma", sum);
+
+average = sum / 10;
+console.log("Media", average);
